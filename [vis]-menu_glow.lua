697d1222f0570bfa2000783476f12cb2a0497435
ThrowTop <119631357+ThrowTop@users.noreply.github.com>
2023-05-16 05:43:04 +0200

    Localized api

--- [vis]-menu_glow.lua
+++ [vis]-menu_glow.lua
@@ -1,8 +1,11 @@
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local client_delay_call, client_set_event_callback, globals_frametime, globals_realtime, math_floor, renderer_gradient, require, ui_get, ui_is_menu_open, ui_menu_position, ui_menu_size, ui_new_color_picker, ui_new_combobox, ui_new_slider, ui_reference, ui_set_callback, ui_set_visible = client.delay_call, client.set_event_callback, globals.frametime, globals.realtime, math.floor, renderer.gradient, require, ui.get, ui.is_menu_open, ui.menu_position, ui.menu_size, ui.new_color_picker, ui.new_combobox, ui.new_slider, ui.reference, ui.set_callback, ui.set_visible
+
 local function lerp_color(c1, c2, t)
-    local r = math.floor(c1[1] * (1 - t) + c2[1] * t + 0.5)
-    local g = math.floor(c1[2] * (1 - t) + c2[2] * t + 0.5)
-    local b = math.floor(c1[3] * (1 - t) + c2[3] * t + 0.5)
-    local a = math.floor(c1[4] * (1 - t) + c2[4] * t + 0.5)
+    local r = math_floor(c1[1] * (1 - t) + c2[1] * t + 0.5)
+    local g = math_floor(c1[2] * (1 - t) + c2[2] * t + 0.5)
+    local b = math_floor(c1[3] * (1 - t) + c2[3] * t + 0.5)
+    local a = math_floor(c1[4] * (1 - t) + c2[4] * t + 0.5)
 
     return { r, g, b, a }
 end
@@ -17,14 +20,14 @@ local function rectangle_gradient(x, y, w, h, precision, t_left, t_right, b_left
             local t = i / w
             local clr = lerp_color(t_left, t_right, t)
             local clr2 = lerp_color(b_left, b_right, t)
-            renderer.gradient(x + i, y, precision, h, clr[1], clr[2], clr[3], clr[4], clr2[1], clr2[2], clr2[3], clr2[4], false)
+            renderer_gradient(x + i, y, precision, h, clr[1], clr[2], clr[3], clr[4], clr2[1], clr2[2], clr2[3], clr2[4], false)
         end
     else
         for i = 0, h - 1, precision do
             local t = i / h
             local clr = lerp_color(t_left, b_left, t)
             local clr2 = lerp_color(t_right, b_right, t)
-            renderer.gradient(x, y + i, w, precision, clr[1], clr[2], clr[3], clr[4], clr2[1], clr2[2], clr2[3], clr2[4], true)
+            renderer_gradient(x, y + i, w, precision, clr[1], clr[2], clr[3], clr[4], clr2[1], clr2[2], clr2[3], clr2[4], true)
         end
     end
 end
@@ -32,7 +35,7 @@ end
 local function hsv_to_rgb(h, s, v, a)
     local r, g, b
 
-    local i = math.floor(h * 6)
+    local i = math_floor(h * 6)
     local f = h * 6 - i
     local p = v * (1 - s)
     local q = v * (1 - f * s)
@@ -66,28 +69,28 @@ end
 local vector = require("vector")
 local tab, cont = "Misc", "Settings"
 local menu = {
-    style = ui.new_combobox(tab, cont, "[ \aAAFF00FFBorder Glow Style\aFFFFFFFF ]", { "-", "gamesense", "Manual" }),
-    [1] = ui.new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
-    [2] = ui.new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
-    [3] = ui.new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
-    [4] = ui.new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
-    thickness = ui.new_slider(tab, cont, "Thickness", 1, 500, 5, true, "px"),
-    speed = ui.new_slider(tab, cont, "Speed", 1, 10, 5, true, "f"),
+    style = ui_new_combobox(tab, cont, "[ \aAAFF00FFBorder Glow Style\aFFFFFFFF ]", { "-", "gamesense", "Manual" }),
+    [1] = ui_new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
+    [2] = ui_new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
+    [3] = ui_new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
+    [4] = ui_new_color_picker(tab, cont, "\n", 255, 255, 255, 255),
+    thickness = ui_new_slider(tab, cont, "Thickness", 1, 500, 5, true, "px"),
+    speed = ui_new_slider(tab, cont, "Speed", 1, 10, 5, true, "f"),
 }
 
-local menu_key = ui.reference("Misc", "Settings", "Menu key")
-local menu_open = ui.is_menu_open()
+local menu_key = ui_reference("Misc", "Settings", "Menu key")
+local menu_open = ui_is_menu_open()
 
 local style
 local function vis_fix(element)
-    style = ui.get(element)
+    style = ui_get(element)
     for i = 1, 4 do
-        ui.set_visible(menu[i], style == "Manual")
+        ui_set_visible(menu[i], style == "Manual")
     end
-    ui.set_visible(menu.thickness, style ~= "-")
-    ui.set_visible(menu.speed, style == "gamesense")
+    ui_set_visible(menu.thickness, style ~= "-")
+    ui_set_visible(menu.speed, style == "gamesense")
 end
-ui.set_callback(menu.style, vis_fix)
+ui_set_callback(menu.style, vis_fix)
 vis_fix(menu.style)
 
 local t = { 0, 0, 0, 0 } -- transparent color
@@ -95,33 +98,33 @@ local rgb_offset = { 0, 0.25, 0.5, 0.75 }
 local fade_alpha = 1
 local key_down = false
 
-client.set_event_callback("paint_ui", function()
+client_set_event_callback("paint_ui", function()
     if style == "-" then return end
-    local p = vector(ui.menu_position())
-    local s = vector(ui.menu_size())
+    local p = vector(ui_menu_position())
+    local s = vector(ui_menu_size())
 
-    local m_key = ui.get(menu_key)
+    local m_key = ui_get(menu_key)
     if not key_down and m_key then
         key_down = true
         menu_open = not menu_open
-        client.delay_call(0.3, function() menu_open = ui.is_menu_open() end)
+        client_delay_call(0.3, function() menu_open = ui_is_menu_open() end)
     end
 
     if not m_key then
         key_down = false
     end
 
-    fade_alpha = lerp(fade_alpha, (menu_open and 1 or 0), globals.frametime() * 1 / 0.05)
+    fade_alpha = lerp(fade_alpha, (menu_open and 1 or 0), globals_frametime() * 1 / 0.05)
     if fade_alpha < 0.05 then return end
 
     local rgb_split_ratio = 1
-    local time = globals.realtime() * (ui.get(menu.speed) / 10) % 1
-    local thickness = ui.get(menu.thickness) + 1
+    local time = globals_realtime() * (ui_get(menu.speed) / 10) % 1
+    local thickness = ui_get(menu.thickness) + 1
 
     local c = {}
     if style == "Manual" then
         for i = 1, 4 do
-            c[i] = { ui.get(menu[i]) }
+            c[i] = { ui_get(menu[i]) }
         end
     else
         for i = 1, 4 do
