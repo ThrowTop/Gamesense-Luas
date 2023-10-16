ce52541cc836f5a6e34a4f9d039328d1a64cf272
ThrowTop <119631357+ThrowTop@users.noreply.github.com>
2023-10-16 18:13:37 +0200

    Create banner_remove.lua

---
+++ banner_remove.lua
@@ -0,0 +1,18 @@
+local IsNewClientAvailable = panorama.loadstring([[
+	var oldClientStatus = NewsAPI.IsNewClientAvailable;
+
+	return {
+		disable: function(){
+			NewsAPI.IsNewClientAvailable = function(){ return false };
+		},
+		restore: function(){
+            NewsAPI.IsNewClientAvailable = oldClientStatus;
+		}
+	}
+]])()
+
+IsNewClientAvailable.disable()
+
+client.set_event_callback("shutdown", function()
+	IsNewClientAvailable.restore()
+end)
